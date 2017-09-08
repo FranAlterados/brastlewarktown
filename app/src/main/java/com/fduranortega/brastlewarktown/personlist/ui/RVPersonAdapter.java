f5dd1b0e50665f59d852eff185dd3840def3735b
Francisco Durán Ortega <fduranortega@gmail.com>
2017-09-08 20:46:02 +0200

    fix: reinvented ui

--- app/src/main/java/com/fduranortega/brastlewarktown/personlist/ui/RVPersonAdapter.java
+++ app/src/main/java/com/fduranortega/brastlewarktown/personlist/ui/RVPersonAdapter.java
@@ -37,13 +37,20 @@ public class RVPersonAdapter extends RecyclerView.Adapter<RVPersonAdapter.Person
 
     @Override
     public void onBindViewHolder(PersonViewHolder holder, int position) {
+        String image = lstPerson.get(position).getPhoto();
         String name = lstPerson.get(position).getName();
         String age = "Age: " + lstPerson.get(position).getAge();
-        String image = lstPerson.get(position).getPhoto();
+        String hairColor = "Hair Color: " + lstPerson.get(position).getHairColor();
+        String weight = "Weight: " + String.format("%.2f", Double.parseDouble(lstPerson.get(position).getWeight())) + " kg";
+        String height = "Height: " + String.format("%.2f", Double.parseDouble(lstPerson.get(position).getHeight())) + " m";
 
-        holder.personName.setText(name);
-        holder.personAge.setText(age);
-        Picasso.with(context).load(image).into(holder.personPhoto);
+        Picasso.with(context).load(image).into(holder.ivPersonPhoto);
+        holder.tvPersonName.setText(name);
+        holder.tvPersonAge.setText(age);
+        holder.tvPersonHairColor.setText(hairColor);
+        holder.tvPersonWeight.setText(weight);
+        holder.tvPersonHeight.setText(height);
+
     }
 
     @Override
@@ -62,17 +69,23 @@ public class RVPersonAdapter extends RecyclerView.Adapter<RVPersonAdapter.Person
     }
 
     public static class PersonViewHolder extends RecyclerView.ViewHolder {
-        CardView cv;
-        TextView personName;
-        TextView personAge;
-        ImageView personPhoto;
+        CardView cvPerson;
+        ImageView ivPersonPhoto;
+        TextView tvPersonName;
+        TextView tvPersonAge;
+        TextView tvPersonHairColor;
+        TextView tvPersonWeight;
+        TextView tvPersonHeight;
 
         PersonViewHolder(View itemView) {
             super(itemView);
-            cv = (CardView) itemView.findViewById(R.id.cvPerson);
-            personName = (TextView) itemView.findViewById(R.id.personName);
-            personAge = (TextView) itemView.findViewById(R.id.personAge);
-            personPhoto = (ImageView) itemView.findViewById(R.id.personPhoto);
+            ivPersonPhoto = (ImageView) itemView.findViewById(R.id.ivPersonPhoto);
+            cvPerson = (CardView) itemView.findViewById(R.id.cvPerson);
+            tvPersonName = (TextView) itemView.findViewById(R.id.tvPersonName);
+            tvPersonAge = (TextView) itemView.findViewById(R.id.tvPersonAge);
+            tvPersonHairColor = (TextView) itemView.findViewById(R.id.tvPersonHairColor);
+            tvPersonWeight = (TextView) itemView.findViewById(R.id.tvPersonWeight);
+            tvPersonHeight = (TextView) itemView.findViewById(R.id.tvPersonHeight);
         }
     }
 }
