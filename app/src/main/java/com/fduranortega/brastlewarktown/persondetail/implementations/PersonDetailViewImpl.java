de1d2df9395b9f518ba123c3d789518915980938
Francisco Durán Ortega <fduranortega@gmail.com>
2017-09-11 01:39:34 +0200

    feat: person detail design start

--- app/src/main/java/com/fduranortega/brastlewarktown/persondetail/implementations/PersonDetailViewImpl.java
+++ app/src/main/java/com/fduranortega/brastlewarktown/persondetail/implementations/PersonDetailViewImpl.java
@@ -2,22 +2,37 @@ package com.fduranortega.brastlewarktown.persondetail.implementations;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.fduranortega.brastlewarktown.R;
 import com.fduranortega.brastlewarktown.model.Person;
 import com.fduranortega.brastlewarktown.persondetail.interfaces.PersonDetailPresenter;
 import com.fduranortega.brastlewarktown.persondetail.interfaces.PersonDetailView;
+import com.squareup.picasso.Picasso;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class PersonDetailViewImpl extends AppCompatActivity implements PersonDetailView {
 
+    @Bind(R.id.ivPhoto)
+    ImageView ivPhoto;
+
+    @Bind(R.id.tvName)
+    TextView tvName;
+
     String id;
+    Person person;
     PersonDetailPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.person_detail);
+
+        ButterKnife.bind(this);
 
         presenter = new PersonDetailPresenterImpl(this);
 
@@ -27,8 +42,10 @@ public class PersonDetailViewImpl extends AppCompatActivity implements PersonDet
 
     @Override
     public void displayData(Person person) {
-        //TODO
-        Toast.makeText(PersonDetailViewImpl.this, person.getName(), Toast.LENGTH_SHORT).show();
+        this.person = person;
+
+        Picasso.with(this).load(person.getPhoto()).into(ivPhoto);
+        tvName.setText(person.getName());
     }
 
     @Override
